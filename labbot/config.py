c245785a037d4db372c09460cae4c6d172666be6
Arya Tayshete <avtayshete_b21@et.vjti.ac.in>
2025-11-30 20:16:02 +0530

    feat: add default units for reference

--- labbot/config.py
+++ labbot/config.py
@@ -1,5 +1,31 @@
-
 DEFAULT_UNITS = {
+    # Biochemical Investigations – Glucose
+    "Fasting Blood Glucose": "mg/dL",
+    "Postprandial Blood Glucose": "mg/dL",
+    "Blood Sugar Random": "mg/dL",
+    "Fasting Urine Glucose": "qualitative",
+    "Postprandial Urine Glucose": "qualitative",
+
+    # Liver Function Tests
+    "Bilirubin Total": "mg/dL",
+    "Bilirubin Direct": "mg/dL",
+    "S.G.O.T": "IU/L",
+    "S.G.P.T": "IU/L",
+    "Alkaline Phosphatase": "IU/L",
+
+    # Renal & Related
+    "Serum Creatinine": "mg/dL",
+    "Blood Urea": "mg/dL",
+    "Blood Urea Nitrogen": "mg/dL",
+    "Serum Uric Acid": "mg/dL",
+    "Serum Calcium": "mg/dL",
+
+    # Electrolytes
+    "Sodium": "mmol/L",
+    "Potassium": "mmol/L",
+    "Chloride": "mmol/L",
+
+    # Haematology / CBC
     "Haemoglobin": "g/dL",
     "RBC Count": "millions/µL",
     "PCV": "%",
@@ -14,4 +40,24 @@ DEFAULT_UNITS = {
     "Monocytes": "%",
     "Eosinophils": "%",
     "Basophils": "%",
+    "ESR": "mm/hr",
+
+    # Inflammatory markers & others
+    "Serum Amylase": "U/L",
+    "RA Factor (Quantitative)": "IU/mL",
+    "C-Reactive Protein": "mg/L",
+    "Absolute Eosinophil Count": "cells/µL",
+
+    # Lipid Profile
+    "Serum Cholesterol": "mg/dL",
+    "HDL Cholesterol": "mg/dL",
+    "LDL Cholesterol": "mg/dL",
+    "Serum Triglycerides": "mg/dL",
+    "VLDL Cholesterol": "mg/dL",
+    "LDL/HDL Ratio": "ratio",
+    "CHOL/HDL Ratio": "ratio",
+
+    # Glycosylated Haemoglobin
+    "Average Blood Glucose": "mg/dL",
+    "HbA1c": "%",
 }
